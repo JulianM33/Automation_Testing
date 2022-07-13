d543224ac1043d8629d7ecfc88c0c069b60948b8
Julian MK <julian.meijerink@gmail.com>
2022-07-13 17:33:58 +0200

    Add login automation tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,23 @@
 describe('empty spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+    const email = 'j.a.m.meijerink@student.tudelft.nl'
+    const password = 'Ferrar123!'
+
+    // Given we are at the login page:
+    cy.visit('https://wave-trial.getbynder.com/login/')
+
+    // When entering correct credentials
+    cy.get('#inputEmail')
+    .type(email)
+
+    cy.get('#inputPassword')
+    .type(password)
+
+    // And click login button
+    cy.get('.login-btn').click()
+
+    // We are logged in
+    cy.url().should('include', '/account/dashboard/')
   })
 })
