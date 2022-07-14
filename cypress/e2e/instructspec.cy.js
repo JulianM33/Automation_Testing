8327d96503df93f86cf8ae53c4e2ef05af6f0337
Julian MK <julian.meijerink@gmail.com>
2022-07-14 10:04:13 +0200

    Add Gherkin and logout test

--- cypress/e2e/instructspec.cy.js
+++ cypress/e2e/instructspec.cy.js
@@ -8,17 +8,34 @@ describe('empty test', () => {
     cy.visit('https://www.instructables.com/account/login/')
     cy.wait(500)
     
+    // When filling in correct credentials
     cy.get('.input-large').first()
     .type(email, {force: true})
 
     cy.get('.input-large').eq(1)
     .type(password, {force: true})
 
+    // And login button is clicked
     cy.get('.btn.btn-large.btn-primary.btn-yellow.auth-action.login').click({force: true})
+    cy.wait(500)
     
-    cy.get('#you-image').click({force: true})
+    // Then we end up on the home page
+    cy.url().should('eq', 'https://www.instructables.com/')
 
+
+    // When we click on our profile
+    cy.get('#you-image').click({force: true})
+    
+    // And we click on logout text 
     cy.get('.btn.btn-default.btn-logout').click({force: true})
+
+    cy.wait(500)
+    
+    // Then we end up on the home page
+    cy.url().should('eq', 'https://www.instructables.com/')
+
+    // And Then we are logged out
+    cy.contains('Log In')
     
   })
 })
