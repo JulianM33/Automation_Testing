b296b8bc41e13e3c97b2b2ad0580eaf77c5d18e1
Julian MK <julian.meijerink@gmail.com>
2022-07-15 09:15:32 +0200

    Increase waiting times for instruct.cy.js

--- cypress/e2e/instructspec.cy.js
+++ cypress/e2e/instructspec.cy.js
@@ -4,9 +4,10 @@ describe('empty test', () => {
     const email = 'user_instruct_4949'
     const password = 'Ferrari123!'
 
+    cy.wait(1000)
     // Given we are at the home page:
     cy.visit('https://www.instructables.com/account/login/')
-    cy.wait(500)
+    cy.wait(5000)
     
     // When filling in correct credentials
     cy.get('.input-large').first()
@@ -17,7 +18,7 @@ describe('empty test', () => {
 
     // And login button is clicked
     cy.get('.btn.btn-large.btn-primary.btn-yellow.auth-action.login').click({force: true})
-    cy.wait(500)
+    cy.wait(5000)
     
     // Then we end up on the home page
     cy.url().should('eq', 'https://www.instructables.com/')
@@ -29,7 +30,7 @@ describe('empty test', () => {
     // And we click on logout text 
     cy.get('.btn.btn-default.btn-logout').click({force: true})
 
-    cy.wait(500)
+    cy.wait(5000)
     
     // Then we end up on the home page
     cy.url().should('eq', 'https://www.instructables.com/')
@@ -48,7 +49,7 @@ describe('empty test', () => {
 
     // And login button is clicked
     cy.get('.btn.btn-large.btn-primary.btn-yellow.auth-action.login').click({force: true})
-    cy.wait(500)
+    cy.wait(2000)
     
     // Then we obtain error message
     cy.contains('Sorry, we can\'t find that account, or your password didn\'t match. Please try again!')
